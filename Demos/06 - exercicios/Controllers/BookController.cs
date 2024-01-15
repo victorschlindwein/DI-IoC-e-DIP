b046a95523a498e79e123b731bbe12cc71103d51
dizzysch <victorwilbert@gmail.com>
2024-01-15 14:29:22 -0300

    desafio: v1 do desafio

--- Demos/06 - exercicios/Controllers/BookController.cs
+++ Demos/06 - exercicios/Controllers/BookController.cs
@@ -1,87 +1,56 @@
-﻿using Dapper;
+﻿using DependencyRoomBooking.Models;
+using DependencyRoomBooking.Repositories.Contracts;
+using DependencyRoomBooking.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
-using RestSharp;
 
 namespace DependencyRoomBooking.Controllers;
 
 [ApiController]
 public class BookController : ControllerBase
 {
+    private readonly ICustomerRepository _customerRepository;
+    private readonly IRoomRepository _roomRepository;
+    private readonly IBookRepository _bookRepository;
+    private readonly IPaymentService _paymentService;
+
+    public BookController(
+        ICustomerRepository customerRepository,
+        IRoomRepository roomRepository,
+        IBookRepository bookRepository, 
+        IPaymentService paymentService
+        )
+    {
+        _customerRepository = customerRepository;
+        _roomRepository = roomRepository;
+        _bookRepository = bookRepository;
+        _paymentService = paymentService;
+    }
+
+    [Route("v1/book")]
+    [HttpPost]
     public async Task<IActionResult> Book(BookRoomCommand command)
     {
-        // Recupera o usuário
-        await using var connection = new SqlConnection();
-        var customer = await connection
-            .QueryFirstOrDefaultAsync<Customer?>("SELECT * FROM [Customer] WHERE [Email]=@email",
-                new { command.Email });
-
+        var customer = await _customerRepository.GetCustomerAsync(command.Email);
         if (customer == null)
             return NotFound();
 
-        // Verifica se a sala está disponível
-        var room = await connection.QueryFirstOrDefaultAsync<Book?>(
-            "SELECT * FROM [Book] WHERE [Room]=@room AND [Date] BETWEEN @dateStart AND @dateEnd",
-            new
-            {
-                Room = command.RoomId,
-                DateStart = command.Day.Date,
-                DateEnd = command.Day.Date.AddDays(1).AddTicks(-1),
-            });
-
-        // Se existe uma reserva, a sala está indisponível
+        var room = await _roomRepository.GetRoomAsync(command.RoomId, command.Day, command.Day.AddDays(5));
         if (room is not null)
             return BadRequest();
 
-        // Tenta fazer um pagamento
-        var client = new RestClient("https://payments.com");
-        var request = new RestRequest()
-            .AddQueryParameter("api_key", "c20c8acb-bd76-4597-ac89-10fd955ac60d")
-            .AddJsonBody(new
-            {
-                User = command.Email,
-                CreditCard = command.CreditCard
-            });
-        var response = await client.PostAsync<PaymentResponse>(request, new CancellationToken());
-
-        // Se a requisição não pode ser completa
+        var response = await _paymentService.Pay(command.Email, command.CreditCard);
         if (response is null)
             return BadRequest();
-
-        // Se o status foi diferente de "pago"
         if (response?.Status != "paid")
             return BadRequest();
 
-        // Cria a reserva
-        var book = new Book(command.Email, command.RoomId, command.Day);
+        var book = await _bookRepository.CreateBookAsync(
+            command.Email,
+            command.RoomId,
+            command.Day,
+            command.Day.AddDays(5)
+        );
 
-        // Salva os dados
-        await connection.ExecuteAsync("INSERT INTO [Book] VALUES(@date, @email, @room)", new
-        {
-            book.Date,
-            book.Email,
-            book.Room
-        });
-
-        // Retorna o número da reserva
-        return Ok();
+        return Ok(book?.RoomId);
     }
 }
-
-public class BookRoomCommand
-{
-    public string Email { get; set; }
-    public Guid RoomId { get; set; }
-    public DateTime Day { get; set; }
-    public CreditCard CreditCard { get; set; }
-}
-
-public record PaymentResponse(int Code, string Status);
-
-public record Customer(string Email);
-
-public record Room(Guid Id, string Name);
-
-public record Book(string Email, Guid Room, DateTime Date);
-
-public record CreditCard(string Number, string Holder, string Expiration, string Cvv);
--- Demos/06 - exercicios/Program.cs
+++ Demos/06 - exercicios/Program.cs
@@ -1,6 +1,19 @@
+using DependencyRoomBooking.Repositories;
+using DependencyRoomBooking.Repositories.Contracts;
+using DependencyRoomBooking.Services;
+using DependencyRoomBooking.Services.Contracts;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<SqlConnection>(x => new SqlConnection());
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
 
 var app = builder.Build();
 
